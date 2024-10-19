bdc70e7adaa4c2320324c22a10fedbc8ea4937c6
Samuel Ortiz <00023020@uca.edu.sv>
2024-10-18 23:11:55 -0600

    :memo: Add swagger initial documentation and schemas

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,86 @@
+import swaggerJSDoc, {Options} from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const swaggerOptions: Options = {
+    definition: {
+        openapi: '3.1.0',
+        info: {
+            title: 'Task tracker',
+            version: '1.0.0',
+            description: 'My beatiful API with JWT'
+        },
+        servers: [
+            {
+                url:'http://localhost:3000/api',
+                description: 'Local server'
+            },
+        ],
+        components: {
+            securitySchemes: {
+              bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT',
+              },
+            },
+            schemas: {
+              Task: {
+                type: 'object',
+                properties: {
+                  title: {
+                    type: 'string',
+                    example: 'Buy groceries',
+                  },
+                  description: {
+                    type: 'string',
+                    example: 'Milk, bread, and eggs',
+                  },
+                },
+              },
+              User: {
+                type: 'object',
+                properties: {
+                  name: {
+                    type: 'string',
+                    example: 'Jonh Doe'
+                  },
+                  email: {
+                    type: 'email',
+                    example: 'jonhdoe@gmail.com'
+                  },
+                  password: {
+                    type: 'string',
+                    example: 'mystrongpassword'
+                  }
+                }
+              },
+              UserLogin: {
+                type: 'object',
+                properties: {
+                  email: {
+                    type: 'string',
+                    example: 'jonhdoe@gmail.com'
+                  },
+                  password: {
+                    type: 'string',
+                    example: 'mystrongpassword'
+                  }
+                }
+              }
+            },
+          },
+          security: [
+            {
+              bearerAuth: [],
+            },
+        ]
+    },
+    apis: ['./src/routes/*.ts']
+}
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+export const setupSwagger = (app: Express): void => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
