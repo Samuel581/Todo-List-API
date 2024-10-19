4c1bf60302dc41de6774af57c20550c05b28e5b7
Samuel Ortiz <00023020@uca.edu.sv>
2024-10-18 23:16:12 -0600

    :memo: Add users and tasks swagger documentation

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,7 +5,44 @@ import { createUserDTO } from "../dtos/user.dto";
 
 const userRouter = Router();
 
+/**
+ * @swagger
+ * /user/register:
+ *  post: 
+ *      summary: Create or register an user
+ *      tags: [User]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses: 
+ *          201: 
+ *              description: User created
+ *          500:
+ *              description: Server error
+ */
 userRouter.post("/register", validateRequest(createUserDTO),registerUser);
-userRouter.get("/login", loginUser);
+
+/**
+ * @swagger
+ * /user/login:
+ *  post: 
+ *      summary: Create or register an user
+ *      tags: [User]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/UserLogin'
+ *      responses: 
+ *          201: 
+ *              description: Token
+ *          500:
+ *              description: Server error
+ */
+userRouter.post("/login", loginUser);
 
 export default userRouter;
