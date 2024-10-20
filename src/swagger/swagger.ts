7b39a429a868d10ba7f99d06d54288871fbfa064
Samuel Ortiz <00023020@uca.edu.sv>
2024-10-19 20:22:59 -0600

    💄 Update swagger CSS

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -2,7 +2,7 @@ import swaggerJSDoc, {Options} from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const swaggerOptions: Options = {
     definition: {
@@ -89,8 +89,6 @@ const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 export const setupSwagger = (app: Express): void => {
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-      customCss:
-          '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
       customCssUrl: CSS_URL,
   }));
 }
