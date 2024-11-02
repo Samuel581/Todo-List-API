dd51784fd6461b02c1dd8a916d4e3d3ad20f3901
Samuel Ortiz <62672937+Samuel581@users.noreply.github.com>
2024-11-02 10:44:23 -0600

    :rocket: Update vercel config files

--- api/index.ts
+++ api/index.ts
@@ -1,26 +1,2 @@
-import express, { Request, Response } from 'express';
-import mainRouter from '../src/routes/main.route';
-import { setupSwagger } from '../src/swagger/swagger';
-import cors from 'cors';
-import serverless from 'serverless-http';
-
-const app = express();
-
-// Enable CORS
-app.use(cors());
-app.use(express.json());
-
-// Setup Swagger
-setupSwagger(app);
-
-// Main routes
-app.use('/api', mainRouter);
-
-// Default route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from the Todo List API!');
-});
-
-// Export the app (no app.listen())
-module.exports = app;
-module.exports.handler = serverless(app);
+import app from "../src";
+export default app;
